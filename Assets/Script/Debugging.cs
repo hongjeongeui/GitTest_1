dee79014ddd04cedc57c617297d84aea02fbf661
hongjeongeui <hjekty@nate.com>
2016-03-19 17:36:57 +0900

    Clear Debug commit

--- Assets/Script/Debugging.cs
+++ Assets/Script/Debugging.cs
@@ -1,20 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Debugging : MonoBehaviour {
+public class Debugging : MonoBehaviour 
+{
 
-	// Use this for initializatio
 
-    private void QAwake()
-    {
-        int from = 0;
-        int to = 1000000000;
-
-        for (float factor = 0.0f; factor <= 1.0f; factor += 0.1f)
-        {
-            int result = (int)(from * (1f - factor) + to * factor);
-
-            Debug.Log(result);
-        }
-    }
 }
